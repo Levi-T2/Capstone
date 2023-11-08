c94d3db51dc151337d58bfdaece6dd52e4b93eb6
Levi Tyler <lttyler713@gmail.com>
2023-11-08 09:35:10 -0700

    Car stuff

--- server/src/services/CarsService.js
+++ server/src/services/CarsService.js
@@ -1,6 +1,19 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 class CarsService {
+    async getCarById(carId) {
+        const car = await dbContext.Cars.findById(carId).populate('creator')
+        if (!car) {
+            throw new BadRequest(`This is not a valid car id: ${carId}`)
+        }
+        return car
+    }
+    async createCar(carData) {
+        const car = await dbContext.Cars.create(carData)
+        await car.populate('creator')
+        return car
+    }
 
     async getAllCars() {
         const cars = dbContext.Cars.find()
