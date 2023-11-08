4a01b8e2731dc44c1ac7054c21b94d83bf1ba664
Levi Tyler <lttyler713@gmail.com>
2023-11-08 12:04:06 -0700

    car model casing fixed

--- server/src/models/Car.js
+++ server/src/models/Car.js
@@ -2,35 +2,35 @@ import { Schema } from "mongoose";
 
 export const CarSchema = new Schema(
     {
-        Model: { type: String, required: true, minLength: 1, maxLength: 50 },
-        Year: { type: Number, required: true, min: 1700, max: 2025 },
-        Mpg: { type: Number, required: true, min: 1, max: 2500 },
+        model: { type: String, required: true, minLength: 1, maxLength: 50 },
+        year: { type: Number, required: true, min: 1700, max: 2025 },
+        mpg: { type: Number, required: true, min: 1, max: 2500 },
         hasTurbo: { type: Boolean, default: false },
         isSupercharged: { type: Boolean, default: false },
-        SeatCapacity: { type: Number, required: true, max: 200 },
-        Horsepower: { type: Number, required: true, min: 1, max: 100000 },
-        Weight: { type: Number, required: true, min: 1, max: 100000 },
-        TrimModel: { type: String, minLength: 1, maxLength: 200 },
-        Displacement: { type: String, minLength: 1, maxLength: 50 },
-        Description: { type: String, required: true, minLength: 1, maxLength: 2000 },
-        Engine: {
+        seatCapacity: { type: Number, required: true, max: 200 },
+        horsepower: { type: Number, required: true, min: 1, max: 100000 },
+        weight: { type: Number, required: true, min: 1, max: 100000 },
+        trimModel: { type: String, minLength: 1, maxLength: 200 },
+        displacement: { type: String, minLength: 1, maxLength: 50 },
+        description: { type: String, required: true, minLength: 1, maxLength: 2000 },
+        engine: {
             type: String, required: true, enum: ['Two-Stroke', 'Four-Stroke', 'I3', 'I4', 'I5', 'I6',
                 'V4', 'V6', 'V8', 'V10', 'V12', 'V16', 'F4', 'Boxer', 'Radial', 'Electric', 'Hybrid', 'Other'],
             default: 'Other'
         },
-        Make: {
+        make: {
             type: String, required: true, enum: ['GMC', 'Ford', 'Chevy', 'Jeep', 'Dodge', 'Chrysler',
                 'Suzuki', 'Toyota', 'Kia', 'Buick', 'BMW', 'Mazda', 'Subaru', 'Hyundai', 'Bentley', 'Volvo',
                 'Mercedes-Benz', 'Porsche', 'Ferrari', 'Lexus', 'Jaguar', 'Audi', 'Lancia', 'Lincoln', 'Nissan',
                 'Honda', 'Fiat', 'Acura', 'VW', 'Infiniti', 'Tesla', 'Cadillac', 'Rolls-Royce', 'Lamborghini', 'Other'],
             default: 'Other'
         },
-        Drivetrain: { type: String, enum: ['4x4', 'RWD', 'FWD', 'AWD'], required: true },
-        BodyType: {
+        drivetrain: { type: String, enum: ['4x4', 'RWD', 'FWD', 'AWD'], required: true },
+        bodyType: {
             type: String, enum: ['Sedan', 'Coupe', 'Roadster', 'SUV', 'Hatchback', 'Van', 'Mini-van', 'Hearse', 'Pickup', '6x6', 'ATV', 'Other'],
             required: true, default: 'Other'
         },
-        FuelType: { type: String, required: true, enum: ['Gas', 'Diesel', 'Hybrid', 'Electric'], default: 'Gas' },
+        fuelType: { type: String, required: true, enum: ['Gas', 'Diesel', 'Hybrid', 'Electric'], default: 'Gas' },
         creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
     },
     {
--- server/src/models/Modification.js
+++ server/src/models/Modification.js
@@ -5,6 +5,19 @@ export const ModificationSchema = new Schema(
         carId: { type: Schema.Types.ObjectId, required: true, ref: 'Car' },
         creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
         description: { type: String, required: true, minLength: 1, maxLength: 2500 },
-        modType: { type: String, required: true, enum: ['Engine', 'Utility', 'Interior', 'Cosmetic', 'Performance', 'Other'], default: 'Other' }
+        modType: { type: String, required: true, enum: ['Engine', 'Utility', 'Interior', 'Cosmetic', 'Performance', 'Other'], default: 'Other' },
+        name: { type: String, required: true, minLength: 1, maxLength: 200 },
+    },
+    {
+        timestamps: true,
+        toJSON: { virtuals: true }
     }
 )
+
+ModificationSchema.virtual('creator',
+    {
+        localField: 'creatorId',
+        foreignField: '_id',
+        ref: 'Account',
+        justOne: true
+    })
