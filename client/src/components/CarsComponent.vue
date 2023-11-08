11a380299e9ea2eb889ca4d590a0918125ea47ea
Jarek Garcia <prodijee@hotmail.com>
2023-11-08 16:20:41 -0700

    Slight Style changes

--- client/src/components/CarsComponent.vue
+++ client/src/components/CarsComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="transparent-bg text-center rounded mt-2 mb-2 text-white">
+    <div class="transparent-bg text-center rounded mb-3 text-white">
 
         <img class="p-4 carPicture"
             src="https://th.bing.com/th/id/OIP.s6zSNqXhqNgBYlU7UKQMOgHaD2?w=336&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="">
