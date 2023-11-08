f5bbc287036d214223c604992e418fa70aeedadd
Jaret Hunemiller <jchunemiller@gmail.com>
2023-11-08 14:27:44 -0700

    car comp adjust

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="container-fluid">
     <section class="row">
-      <div v-for="car in cars" :key="car.id" class="col-3 p-5">
+
+      <div class="text-center">
+
+
+        <div class="col-12 d-flex justify-content-around pt-3">
+
+          <button class="btn fs-5 transparent-bg text-light">
+            User's Cars <i class="mdi mdi-car"></i>
+          </button>
+
+          <button class="btn fs-5 transparent-bg text-light">
+            Filter By <i class="mdi mdi-arrow-down-drop-circle-outline"></i>
+          </button>
+        </div>
+
+      </div>
+
+
+      <div v-for="car in cars" :key="car.id" class="col-12 col-md-4 p-5">
         <CarsComponent :carProp="car" />
       </div>
     </section>
@@ -36,4 +54,8 @@ export default {
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.transparent-bg {
+  background-color: rgba(0, 0, 0, 0.278);
+}
+</style>
