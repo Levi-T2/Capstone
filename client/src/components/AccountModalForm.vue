71786e801814b517d4f5d86b9c56d5e8f9874862
Cody <qtipcody@gmail.com>
2023-11-09 12:21:12 -0700

    edit Account is working

---
+++ client/src/components/AccountModalForm.vue
@@ -0,0 +1,82 @@
+<template>
+<div class="modal fade" id="AccountModalForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content formbg formbrdr">
+    <div class="modal-header">
+    <h5 class="modal-title text-light" id="exampleModalLabel">Edit Account</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+        <form @submit.prevent="editAccount()">
+
+    <div class="mb-3 text-light">
+    <label for="name" class="form-label">Account Name</label>
+    <input v-model="editable.name" type="name" class="form-control" id="name">
+    </div>
+
+    <div class="mb-3 text-light">
+    <label for="picture" class="form-label">Account Picture</label>
+    <input v-model="editable.picture" type="picture" class="form-control" id="picture">
+    </div>
+    
+    <div class="mb-3 text-light">
+    <label for="bio" class="form-label">Account bio</label>
+    <input v-model="editable.bio" type="bio" class="form-control" id="bio">
+    </div>
+
+    <button type="submit">Save</button>
+        </form>
+    </div>
+    </div>
+</div>
+</div>
+</template>
+
+
+<script>
+import { computed, ref, watchEffect } from 'vue';
+import { AppState } from '../AppState.js';
+import Pop from '../utils/Pop.js';
+import { logger } from '../utils/Logger.js';
+import { accountService } from '../services/AccountService.js';
+import { Modal } from 'bootstrap';
+
+
+export default {
+    setup(){
+        const editable = ref({})
+
+        watchEffect(()=> {
+            editable.value = AppState.account
+        })
+
+
+    return { 
+        editable,
+        account: computed(()=> AppState.account),
+
+        async editAccount(){
+            try {
+                const accountData = editable.value
+                logger.log(accountData)
+                await accountService.editAccount(accountData)
+                Modal.getOrCreateInstance('#AccountModalForm').hide()
+            } catch (error) {
+                Pop.error(error)
+            }
+        }
+    }
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.formbg {
+    background-color: rgba(0, 0, 128, 0.705);
+}
+
+.formbrdr {
+    border: 5px solid blue;
+}
+</style>
--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -3,18 +3,20 @@
     <div class="about text-center">
       <div class="row mt-3 justify-content-center">
         <div class="col-12 col-md-10 bg-dark text-center">
-          <div class="col-12 col-md-3">
-            <div>
+          <div class="col-12">
+            <div class="mt-3 text-center">
               <h1>{{ account.name }}</h1>
               <img class="rounded-circle my-3" :src="account.picture" alt="" />
             </div>
             <div>
-              <p>I love cars and going fast</p>
+              <p>{{ account.bio }}</p>
             </div>
           </div>
 
           <div class="text-end p-4">
-            <button  class="mdi mdi-pen-plus btn btn-outline-light">Edit Account</button>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#AccountModalForm">
+              Edit Account
+            </button>
           </div>
 
         </div>
@@ -24,32 +26,37 @@
 </div>
     </div>
 
-    <div class="row text-center justify-content-around">
+    <div  class="row text-center justify-content-around">
 
       <div class="col-10  bg-dark my-3">
         <h1 class="fs-3">My Cars</h1> 
       </div>
 
-      <div class="col-10 bg-dark my-3"
+      <div class="col-10 bg-dark my-3 "
       ><h1 class="fs-3">My Favorites</h1> 
+      
+      
       </div>
 
 
 
     </div>
   </div>
+  <AccountModalForm/>
 </template>
 
 <script>
 import { computed } from 'vue';
 import { AppState } from '../AppState';
+
 export default {
-  setup() {
-    return {
-      account: computed(() => AppState.account),
-      cars: computed(()=> AppState.cars)
-    }
-  }
+    setup() {
+        return {
+            account: computed(() => AppState.account),
+            cars: computed(() => AppState.cars)
+        };
+    },
+
 }
 </script>
 
