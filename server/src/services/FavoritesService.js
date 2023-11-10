9de9518c181f4fbe7b7b0a3262a52814c13b2df7
Levi Tyler <lttyler713@gmail.com>
2023-11-10 10:22:21 -0700

    Favorite controller set up

---
+++ server/src/services/FavoritesService.js
@@ -0,0 +1,18 @@
+
+class FavoritesService {
+    destroyFavorite(favId, userId) {
+        throw new Error("Method not implemented.");
+    }
+    postFavorite(favData) {
+        throw new Error("Method not implemented.");
+    }
+    getFavoriteById() {
+        throw new Error("Method not implemented.");
+    }
+    getAllFavorites() {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+export const favoritesService = new FavoritesService()
