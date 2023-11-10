8a014161e233777190afc48c8be6be32722db93d
Jaret Hunemiller <jchunemiller@gmail.com>
2023-11-10 09:48:25 -0700

    Hope it fixed

--- client/src/pages/ModListPage.vue
+++ client/src/pages/ModListPage.vue
@@ -7,16 +7,7 @@
         </section>
         <section v-if="mods" class="row">
             <div v-for="mod in mods" :key="mod.id" class="col-12 col-md-6 p-2">
-<<<<<<< HEAD
-                <div class="bg-dark rounded p-2">
-                    <img :src="mod.imgUrl" alt="">
-                    <p>{{ mod.name }}</p>
-                    <p>{{ mod.description }}</p>
-                    <p>Mod Type: {{ mod.modType }}</p>
-                </div>
-=======
-               <ModsComponent :mod="mod"></ModsComponent>
->>>>>>> e03b729b4668a5699c2ad5834fbf3d87ab26055c
+                <ModsComponent :mod="mod"></ModsComponent>
             </div>
         </section>
     </div>
@@ -34,12 +25,7 @@ import ModsComponent from '../components/ModsComponent.vue';
 
 export default {
     setup() {
-<<<<<<< HEAD
-        const route = useRoute()
-
-=======
         const route = useRoute();
->>>>>>> e03b729b4668a5699c2ad5834fbf3d87ab26055c
         onMounted(() => {
             getModsByCarId();
         });
@@ -54,14 +40,9 @@ export default {
         }
         return {
             mods: computed(() => AppState.activeModList)
-<<<<<<< HEAD
-        }
-    }
-=======
         };
     },
     components: { ModsComponent }
->>>>>>> e03b729b4668a5699c2ad5834fbf3d87ab26055c
 };
 </script>
 
