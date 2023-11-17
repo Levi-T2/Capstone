36cbcdb7cd3160e30d43e7bb8f6008c61f1b01ec
Levi Tyler <lttyler713@gmail.com>
2023-11-17 09:59:47 -0700

    About to Style the About Page

--- client/src/components/PageChanger.vue
+++ client/src/components/PageChanger.vue
@@ -2,11 +2,11 @@
     <section class="row">
         <div class="page-changer d-flex justify-content-between align-items-center">
             <div>
-                <button v-if="page <= 0" disabled @click="changePage(page -= 1)" class="btn btn-light rounded-circle m-1"
-                    title="To Previous Page">
+                <button v-if="page <= 0" disabled @click="changePage(page -= 1)"
+                    class="btn btn-light btn-page-prev  rounded-circle m-1" title="To Previous Page">
                     <i class="mdi mdi-arrow-left"></i>
                 </button>
-                <button v-else @click="changePage(page -= 1)" class="btn btn-light rounded-circle m-1"
+                <button v-else @click="changePage(page -= 1)" class="btn btn-light btn-page-prev rounded-circle m-1"
                     title="To Previous Page">
                     <i class="mdi mdi-arrow-left"></i>
                 </button>
@@ -16,10 +16,11 @@
             </div>
             <div>
                 <button v-if="totalCars < 1" disabled @click="changePage(page += 1)"
-                    class="btn btn-light rounded-circle m-1" title="To Next Page">
+                    class="btn btn-light rounded-circle m-1 btn-page-next" title="To Next Page">
                     <i class="mdi mdi-arrow-right"></i>
                 </button>
-                <button v-else @click="changePage(page += 1)" class="btn btn-light rounded-circle m-1" title="To Next Page">
+                <button v-else @click="changePage(page += 1)" class="btn btn-light rounded-circle m-1 btn-page-next"
+                    title="To Next Page">
                     <i class="mdi mdi-arrow-right"></i>
                 </button>
             </div>
@@ -59,5 +60,29 @@ export default {
     background-color: rgba(0, 0, 0, 0.278);
     color: white;
     border-radius: 70px;
+}
+
+
+// Styles and on-hovers for the buttons
+.btn-page-next {
+    border: 1px solid rgba(220, 220, 220, 0);
+    transition: ease-in-out 0.275s;
+}
+
+.btn-page-next:hover,
+.btn-page-next:focus {
+    box-shadow: inset 6.5em 0 0 0 rgb(6, 161, 6);
+    border: 1px solid gainsboro;
+}
+
+.btn-page-prev {
+    border: 1px solid rgba(220, 220, 220, 0);
+    transition: ease-in-out 0.275s;
+}
+
+.btn-page-prev:hover,
+.btn-page-prev:focus {
+    box-shadow: inset 6.5em 0 0 0 rgb(167, 5, 5);
+    border: 1px solid gainsboro;
 }
 </style>
