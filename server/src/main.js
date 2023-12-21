3646fecbec0212f1bb30a3b74e4a145183bb2e09
Levi <lttyler713@gmail.com>
2023-12-21 14:30:16 -0700

    Should Work For Live

--- server/src/main.js
+++ server/src/main.js
@@ -7,7 +7,7 @@ import { logger } from './utils/Logger'
 
 // create server & socketServer
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 if (process.env.NODE_ENV == 'dev') {
   // @ts-ignore
